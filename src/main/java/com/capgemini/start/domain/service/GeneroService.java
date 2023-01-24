70a9e4fd375f723c7b2a72529f216c26a4416a9b
Ana Paula Gatti <100304320+Ana-Gatti@users.noreply.github.com>
2023-01-24 09:58:22 -0300

    Etapa 1 do Trello

---
+++ src/main/java/com/capgemini/start/domain/service/GeneroService.java
@@ -0,0 +1,14 @@
+package com.capgemini.start.domain.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class GeneroService extends AbstractService {
+
+	@Override
+	protected JpaRepository getRepository() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
